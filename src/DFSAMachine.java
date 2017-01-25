0e97ee51d8c19561ff34d154688dd20ad836c328
amjoshi <amjoshi@cpp.edu>
2017-01-25 02:13:36 -0800

    works without metachar

--- src/DFSAMachine.java
+++ src/DFSAMachine.java
@@ -8,51 +8,58 @@ public class DFSAMachine {
 	public static void main(String[]args){
 		String fileName= "InputFile.txt";
 		ArrayList<String> input = new ArrayList<String>();
+		
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			FSA fsa = new FSA();
-			int numStates = Integer.parseInt(br.readLine());
-			fsa.setNumStates(numStates);
-			fsa.printNumStates(); 
-
-			ArrayList<String> finalStates = new ArrayList<String>(); 
-			String [] numFinal = br.readLine().split(" "); 
-			for (String s: numFinal){
-				finalStates.add(s);
-			}
-			fsa.setFinalStates(finalStates);
-			fsa.printFinalStates();
-			ArrayList<String> alphabet = new ArrayList<String>(); 
-			String [] alphabet1 = br.readLine().split(" "); 
-			for (String s: alphabet1){
-				alphabet.add(s);
-			}
-			fsa.setAlphabet(alphabet);
-			fsa.printAlphabet();
-			HashMap <String , String > transitions = new HashMap<String, String>(); 
-			String line = br.readLine();
-			do {
-				
-				//System.out.println("Transition " + line);
-
-				String [] transitionParts = line.split(" ");
-			//	System.out.println(transitionParts[0] + transitionParts[1] + transitionParts[2]);
-				transitions.put(transitionParts[0].charAt(1)+transitionParts[1], transitionParts[2].charAt(0)+"");
-				line = br.readLine();
-			}while (line.contains("("));
-			fsa.setTransitions(transitions);
-			fsa.printTransitions();
-			ArrayList<String> inputVal = new ArrayList<String>();
+			int numInput = 1;
 			do{
-				System.out.print("Input: "+ line +"\t");
-				System.out.println(fsa.works(line));
-				line = br.readLine();
-			} while (line.equals("******************")==false);
-			System.out.println(line );
+				System.out.println("Finite State Automaton #" + numInput);
+				FSA fsa = new FSA();
+				int numStates = Integer.parseInt(br.readLine());
+				fsa.setNumStates(numStates);
+				fsa.printNumStates(); 
+
+				ArrayList<String> finalStates = new ArrayList<String>(); 
+				String [] numFinal = br.readLine().split(" "); 
+				for (String s: numFinal){
+					finalStates.add(s);
+				}
+				fsa.setFinalStates(finalStates);
+				fsa.printFinalStates();
+				ArrayList<String> alphabet = new ArrayList<String>(); 
+				String [] alphabet1 = br.readLine().split(" "); 
+				for (String s: alphabet1){
+					alphabet.add(s);
+				}
+				fsa.setAlphabet(alphabet);
+				fsa.printAlphabet();
+				HashMap <String , String > transitions = new HashMap<String, String>(); 
+				String line = br.readLine();
+				do {
+
+					//System.out.println("Transition " + line);
+
+					String [] transitionParts = line.split(" ");
+					//	System.out.println(transitionParts[0] + transitionParts[1] + transitionParts[2]);
+					transitions.put(transitionParts[0].charAt(1)+transitionParts[1], transitionParts[2].charAt(0)+"");
+
+					line = br.readLine();
+				}while (line.contains("("));
+				fsa.setTransitions(transitions);
+				fsa.printTransitions();
+				ArrayList<String> inputVal = new ArrayList<String>();
+				do{
+					System.out.print("Input: "+ line +"\t");
+					System.out.println(fsa.works(line));
+					line = br.readLine();
+				} while (line.equals("******************")==false);
+				System.out.println(line );
+				numInput++;
+			}while(br.ready());
 
 		}catch(Exception e){
 
 		}
 	}
-	
+
 }
--- src/FSA.java
+++ src/FSA.java
@@ -83,6 +83,7 @@ public class FSA {
 		}
 		return true;
 	}
+	
 
 
 
